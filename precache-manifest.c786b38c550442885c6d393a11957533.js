06e107d3f6841047d2a19649d457d1126930c7fa
KIRANNAIR1 <kirannair17@gmail.com>
2019-04-07 14:10:31 +0530

    Updates

---
+++ precache-manifest.c786b38c550442885c6d393a11957533.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "219d1ffc95ec6e4951de",
+    "url": "/reactclient/static/js/main.2e8d3b1f.chunk.js"
+  },
+  {
+    "revision": "80a3e7d99ec31d9434b7",
+    "url": "/reactclient/static/js/runtime~main.03e39d6a.js"
+  },
+  {
+    "revision": "5ce98d6604cbfc60d1aa",
+    "url": "/reactclient/static/js/2.ea35c62a.chunk.js"
+  },
+  {
+    "revision": "5fe4cbe8a4e3a57879fa34f6ccee9bb3",
+    "url": "/reactclient/index.html"
+  }
+];
